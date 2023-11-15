cb5775c13605c5e3b46e356baacff6dc197543ac
jennanoh <jennanoh28@gmail.com>
2023-11-15 16:15:39 -0500

    data model created w dummy data

---
+++ src/app/ishowsearch.ts
@@ -0,0 +1,9 @@
+export interface IShowsearch {
+  name: string,
+  summary: string,
+  network: string,
+  image: string,
+  rating: number,
+  genre: string,
+  date: string,
+}
--- src/app/show-search/show-search.component.ts
+++ src/app/show-search/show-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IShowsearch } from '../ishowsearch';
 
 @Component({
   selector: 'app-show-search',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './show-search.component.css'
 })
 export class ShowSearchComponent {
-
+  current: IShowsearch = {
+    name: 'Friends',
+    summary: 'Show about friends',
+    network: 'NBC',
+    image: ' ',
+    rating: 8.9,
+    genre: 'Comedy',
+    date: '1994-09-22',
+  }
 }
