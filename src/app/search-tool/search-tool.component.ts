e3cb61fe5f6350ae462c60890d5f7fd979602b71
jennanoh <jennanoh28@gmail.com>
2023-12-01 11:49:54 -0500

    debounce time and validator added to search tool

--- src/app/search-tool/search-tool.component.ts
+++ src/app/search-tool/search-tool.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-tool',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './search-tool.component.css'
 })
 export class SearchToolComponent {
+  search = new FormControl('', [Validators.minLength(3)])
 
-}
+constructor(){
+  this.search.valueChanges
+  .pipe(debounceTime(1000))
+  .subscribe(searchValue => {
+    if (this.search.valid)
+    console.log(searchValue)
+  }
+  )}
+  }
+
+
